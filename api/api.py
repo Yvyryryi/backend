91886d7a36e6b85968b861efd6d33a60c10e0b22
Jorgedavyd <jorged.encyso@gmail.com>
2024-10-06 08:04:08 -0300

    setting up for local attempt

--- api/api.py
+++ api/api.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+import requests
 
 app = FastAPI()
-
-class Item(BaseModel):
-    date: str
-    v: float
-    ### ...
 
 @app.get("/yvyryryi/download/{init_date}-{final_date}", response_model=...)
 async def download(init_date: str, final_date: str):
@@ -18,7 +14,18 @@ async def download(init_date: str, final_date: str):
 
 @app.get("/yvyryryi/visualize/{init_date}-{final_date}", response_model=List[Item])
 async def visualize(init_date: str, final_date: str):
-    item = ...
+    item = requests.get(
+        'http://localhost:8888/druid/v2/sql',
+        headers = {'Content-Type' : 'application/json'},
+        data = {
+            "query": f"SELECT * FROM yvyryryi WHERE {init_date} < date < {final_date}",
+            "context" : {"sqlQueryId" : "request01"},
+            "header" : True,
+            "typesHeader" : True,
+            "sqlTypesHeader" : True,
+        }
+    )
+    ## {rel_time: ..., velocity: ...}
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
